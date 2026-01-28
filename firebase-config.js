575ffd036b0d1838a11ef5d2717989a30a2d2e16
IlyaSigma111 <ilyagulkov25@gmail.com>
2026-01-28 18:44:10 +0500

    Update firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase конфигурация
+const firebaseConfig = {
+    apiKey: "AIzaSyC9OSllGc8U-au0281HfikJkI5caDkqOYc",
+    authDomain: "goydacloud.firebasestorage.app",
+    databaseURL: "https://goydacloud-default-rtdb.firebaseio.com",
+    projectId: "goydacloud",
+    storageBucket: "goydacloud.firebasestorage.app",
+    messagingSenderId: "937429390580",
+    appId: "1:937429390580:web:7be76b6755a07ff6ae7aa1"
+};
